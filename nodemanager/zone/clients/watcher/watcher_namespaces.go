1014fd3834282a709eb94c8937c594a0b60bac79
huyuepeng <huyuepeng@bonc.com.cn>
2017-03-19 18:48:02 -0700

    伪造namespaces数据

--- nodemanager/zone/clients/watcher/watcher_namespaces.go
+++ nodemanager/zone/clients/watcher/watcher_namespaces.go
@@ -41,6 +41,7 @@ type EndpointObject struct {
 type EndpointMetadata struct {
 	Name      string
 	Namespace string
+	Labels    map[string]string
 }
 
 type EndpointSubset struct {
@@ -89,7 +90,7 @@ func getNamespacesFromK8s(url string) (namespaces []string) {
 	return
 }
 
-func getAppName(obj EndpointObject) (appName string) {
+func getAppName(obj EndpointObject, jobZoneType string) (appName string) {
 	if len(obj.Subsets) == 0 {
 		return
 	}
@@ -98,19 +99,17 @@ func getAppName(obj EndpointObject) (appName string) {
 		return
 	}
 
-	//	zone := getZoneType() //"dmz"
-
-	//	if obj.Metadata.Labels[zone] == zone {
-	appName = obj.Metadata.Name
-	//	}
+	if obj.Metadata.Labels[jobZoneType] == jobZoneType {
+		appName = obj.Metadata.Name
+	}
 
 	return
 }
 
-func getAppListFromEpList(epList EndpointsList) (appList []string) {
+func getAppListFromEpList(epList EndpointsList, jobZoneType string) (appList []string) {
 	for _, object := range epList.Items {
 		//获取本租户下的一个ep对象所对应的app信息(1个)
-		appName := getAppName(object)
+		appName := getAppName(object, jobZoneType)
 		if appName != "" {
 			appList = append(appList, appName)
 		}
@@ -120,7 +119,7 @@ func getAppListFromEpList(epList EndpointsList) (appList []string) {
 }
 
 //从k8s集群获取租户的详细信息
-func getNamespacesDetailInfoFromK8s(getNamespacesURL string) (namespacesDetail NamespacesDetailInfo) {
+func getNamespacesDetailInfoFromK8s(getNamespacesURL string, jobZoneType string) (namespacesDetail NamespacesDetailInfo) {
 	namespacesList := getNamespacesFromK8s(getNamespacesURL)
 	namespacesDetail.NamespacesList = namespacesList
 
@@ -129,7 +128,7 @@ func getNamespacesDetailInfoFromK8s(getNamespacesURL string) (namespacesDetail N
 		//拿到本租户下的所有ep(epList中包含了服务名N个)
 		endpointList := getServiceFromK8s(getEndpointsURL)
 		//解析epList 将N个服务的名字解析出来{"app1","app2",..."appN"}
-		namespacesDetail.NamespacesAppList = append(namespacesDetail.NamespacesAppList, getAppListFromEpList(endpointList))
+		namespacesDetail.NamespacesAppList = append(namespacesDetail.NamespacesAppList, getAppListFromEpList(endpointList, jobZoneType))
 	}
 
 	return
