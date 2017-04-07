f5ddb02e4c30e7a63a2b0e6d9f80b7d3d55d1b64
Molen Zhang <zhangfeng1@bonc.com.cn>
2017-04-07 16:39:53 +0800

    修改默认地址bug

--- config/config.go
+++ config/config.go
@@ -9,6 +9,7 @@ import (
 
 //NgFrontCfgInfo ngfront程序启动参数以及其他配置信息结构
 type NgFrontCfgInfo struct {
+	ListenIP        string
 	ListenPort      string
 	HeartCycle      time.Duration
 	HeartServerAddr string
@@ -28,6 +29,9 @@ const DefaultHeartTimeout = DefaultHeartCycle * 3
 //DefaultHeartServerPath 默认心跳服务器path
 const DefaultHeartServerPath = "/ngfront/heart"
 
+//DefaultListenIP
+const DefaultListenIP = "localhost"
+
 //DefaultListenPort 默认监听端口
 const DefaultListenPort = "8083"
 
@@ -36,13 +40,14 @@ const DefaultLogLevel = "info"
 
 // Init 初始配置参数
 func Init() {
+	flag.StringVar(&NgFrontCfg.ListenIP, "ip]", DefaultListenIP, "默认监听地址")
 	flag.StringVar(&NgFrontCfg.ListenPort, "port", DefaultListenPort, "默认监听端口")
 	flag.DurationVar(&NgFrontCfg.HeartCycle, "heartcycle", DefaultHeartCycle, "默认心跳间隔 单位/秒")
 	flag.StringVar(&NgFrontCfg.HeartServerAddr, "heartserveraddr", "http://localhost:8083"+DefaultHeartServerPath, "默认心跳服务器地址")
 	flag.StringVar(&NgFrontCfg.LogLevel, "loglevel", DefaultLogLevel, "默认日志级别，支持debug,info,warn,error,fatal")
 	flag.Parse()
 
-	NgFrontCfg.HeartServerAddr = "http://localhost:" + NgFrontCfg.ListenPort + DefaultHeartServerPath
+	NgFrontCfg.HeartServerAddr = "http://" + NgFrontCfg.ListenIP + NgFrontCfg.ListenPort + DefaultHeartServerPath
 
 	return
 }
