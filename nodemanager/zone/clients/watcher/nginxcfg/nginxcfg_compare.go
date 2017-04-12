3341ff4b2eaabc0cd07afc83d389ad1ce50361dc
root <root@localhost.localdomain>
2017-04-11 20:21:05 -0700

    增加了对比配置的逻辑

--- nodemanager/zone/clients/watcher/nginxcfg/nginxcfg_compare.go
+++ nodemanager/zone/clients/watcher/nginxcfg/nginxcfg_compare.go
@@ -2,10 +2,17 @@ package nginxcfg
 
 import (
 	//"fmt"
+	"encoding/json"
 	"github.com/emicklei/go-restful"
+	"ngfront/communicate"
 	"ngfront/logdebug"
+	"ngfront/nodemanager/nodes"
 	"strings"
 )
+
+func compareContent(clientA, clientB map[string]map[string]KubeNGConfig) bool {
+	return true
+}
 
 func (svc *ServiceInfo) compareAllWatchersNginxCfgs(request *restful.Request, response *restful.Response) {
 	twoClientIDs := request.PathParameter("clientA-clientB")
@@ -32,5 +39,70 @@ func (svc *ServiceInfo) compareSingleWatchersNginxCfgs(request *restful.Request,
 
 	logdebug.Println(logdebug.LevelDebug, "对比的watcherID = ", watcherID)
 
+	clientANginxCfgURL, clientBNginxCfgURL := getNginxCfgByWatcherURL(clientsSet, watcherID, request, response)
+
+	respA, _ := communicate.SendRequestByJSON(communicate.GET, clientANginxCfgURL, nil)
+	clientAMap := make(map[string]map[string]KubeNGConfig, 0)
+	json.Unmarshal(respA, &clientAMap)
+
+	logdebug.Println(logdebug.LevelDebug, "根据watcherID获取的数据", clientAMap)
+
+	respB, _ := communicate.SendRequestByJSON(communicate.GET, clientBNginxCfgURL, nil)
+	clientBMap := make(map[string]map[string]KubeNGConfig, 0)
+	json.Unmarshal(respB, &clientBMap)
+
+	logdebug.Println(logdebug.LevelDebug, "根据watcherID获取的数据", clientBMap)
+
+	result := compareContent(clientAMap, clientBMap)
+
+	response.WriteHeaderAndJson(200, result, "application/json")
+
 	return
+}
+
+func getNginxCfgByWatcherURL(clientsSet []string, watcherID string, request *restful.Request, response *restful.Response) (clientAURL string, clientBURL string) {
+	request.Request.ParseForm()
+	clientA := nodes.ClientInfo{
+		NodeIP:   request.Request.Form.Get("NodeIPA"),
+		ClientID: clientsSet[0],
+	}
+
+	appSrcType := request.Request.Form.Get("AppSrcType")
+
+	key := clientA.CreateKey()
+	clientInfo := nodes.GetClientInfo(key)
+
+	clientANginxCfgURL := "http://" +
+		clientInfo.NodeIP +
+		clientInfo.APIServerPort +
+		"/" +
+		clientInfo.NginxCfgsAPIServerPath +
+		"/" +
+		appSrcType +
+		"/" +
+		watcherID
+
+	logdebug.Println(logdebug.LevelDebug, "从kubeng获取ClientA的配置URL=", clientANginxCfgURL)
+
+	clientB := nodes.ClientInfo{
+		NodeIP:   request.Request.Form.Get("NodeIPB"),
+		ClientID: clientsSet[1],
+	}
+
+	key = clientB.CreateKey()
+	clientInfo = nodes.GetClientInfo(key)
+
+	clientBNginxCfgURL := "http://" +
+		clientInfo.NodeIP +
+		clientInfo.APIServerPort +
+		"/" +
+		clientInfo.NginxCfgsAPIServerPath +
+		"/" +
+		appSrcType +
+		"/" +
+		watcherID
+
+	logdebug.Println(logdebug.LevelDebug, "从kubeng获取ClientA的配置URL=", clientBNginxCfgURL)
+
+	return clientANginxCfgURL, clientBNginxCfgURL
 }
--- template/js/nginx/area.js
+++ template/js/nginx/area.js
@@ -44,8 +44,8 @@
  });/*reday*/
  
 function showAreaTable(option){
-	var areaIP = "localhost";
-	var areaPort = "port";
+	var areaIP = "192.168.252.138";
+	var areaPort = "8011";
 	var areaUrl = "http://"+areaIP+":"+areaPort+"/ngfront/zone";
 	var clientsUrl = "http://"+areaIP+":"+areaPort+"/ngfront/zone/clients?areaType=";
 	$.ajax({
@@ -89,8 +89,8 @@ function showAreaTable(option){
 
 function areaRefresh(){
 	//location.replace(location.href);
-	var areaIP = "localhost";
-	var areaPort = "port";
+	var areaIP = "192.168.252.138";
+	var areaPort = "8011";
 	location.href = "http://"+areaIP+":"+areaPort+"/ngfront";
 }
 
